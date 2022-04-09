d89d37c4cd71eff63aab98caf16dde2ee2ea131f
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-04-09 23:14:24 +0900

    황인호 수정

--- renders/index.js
+++ renders/index.js
@@ -3,6 +3,7 @@ const loginRouter = require('./login')
 const registerRouter = require('./register')
 const writeRouter = require('./write')
 const detailRouter = require('./detail')
+const detail_mapRouter = require('./detail_map')
 const mainRouter = require('./main')
 
 const router = express.Router()
@@ -11,6 +12,7 @@ router.use('/', loginRouter)
 router.use('/user/register', registerRouter)
 router.use('/user/main', mainRouter)
 router.use('/user/detail', detailRouter)
+router.use('/user/detail_map', detail_mapRouter)
 router.use('/user/write', writeRouter)
 
 module.exports = router 
